71519a768c55799735b0c33dec324536364e2568
jesse springman <jessebarbosa45@gmail.com>
2024-08-23 18:21:16 -0300

    testando resposta do prisma

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const port = 3000;
+const prisma = new PrismaClient();
 
-app.get('/movies', (req,res)=>{
-    console.log('Lista fimes');
-    
-})
+app.get('/movies', async (req,res)=>{
+  const movies = await prisma.movie.findMany();
+    res.json(movies);
+});
+
 
 app.listen(port, ()=>{
     console.log(`Servidor em execução ${port}`);
     
-})
+});
