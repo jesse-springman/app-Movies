50f0ef3e1add5cddfb29e8585b2e9395a77ceb5a
jesse springman <jessebarbosa45@gmail.com>
2024-08-27 19:31:49 -0300

    add ordenação filme

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,21 @@ const port = 3000;
 const prisma = new PrismaClient();
 
 app.get('/movies', async (req,res)=>{
-  const movies = await prisma.movie.findMany();
+  const movies = await prisma.movie.findMany({
+    orderBy:{
+        title:'asc'
+    },
+
+    include:{
+        genres:true,
+        language_movie: true
+    },
+  });
+
     res.json(movies);
 });
+
+
 
 
 app.listen(port, ()=>{
