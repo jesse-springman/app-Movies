4e11bcb928c437a2a1cad0e1d76b2093ce76f1e8
jesse springman <jessebarbosa45@gmail.com>
2024-08-28 20:15:19 -0300

    rota para  add filmes

--- src/server.ts
+++ src/server.ts
@@ -5,25 +5,52 @@ const app = express();
 const port = 3000;
 const prisma = new PrismaClient();
 
-app.get('/movies', async (req,res)=>{
+app.use(express.json());
+
+app.get('/movies', async (req, res) => {
   const movies = await prisma.movie.findMany({
-    orderBy:{
-        title:'asc'
+
+    orderBy: {
+      title: 'asc'
+
     },
 
-    include:{
-        genres:true,
-        language_movie: true
+    include: {
+      genres: true,
+      // language_movie: true
+
     },
   });
 
-    res.json(movies);
+  res.json(movies);
+});
+
+app.post('/movies', async (req, res) => {
+
+  const { title, genre_id, language_id, realese_data, oscar_count } = req.body;
+
+try{
+  await prisma.movie.create({
+    data: {
+      title,
+      genre_id,
+      language_id,
+      realese_data: new Date( realese_data),
+      oscar_count
+    }
+  });
+
+  res.status(201).send();
+}
+
+catch(error){
+  return res.status(500).send({message : 'Falha ao cadastrar um filme'});
+}
+
 });
 
 
+app.listen(port, () => {
+  console.log(`Servidor em execução ${port}`);
 
-
-app.listen(port, ()=>{
-    console.log(`Servidor em execução ${port}`);
-    
 });
